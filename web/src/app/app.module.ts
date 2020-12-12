1fd735e2dbd8500378588a486eda99ffc13fd325
Max Levitskiy <max@webtree.org>
2020-12-12 10:00:23 +0100

    Add standard theme

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,16 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {ProfileLogoComponent} from "./profile-logo/profile-logo.component";
+import {TokenService} from "./_services/token.service";
+import {AuthService} from "./_services/auth.service";
+import {AlertService} from "./_services/alert.service";
+import {MatButtonModule} from "@angular/material/button";
+import {MatMenuModule} from "@angular/material/menu";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileLogoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    TokenService,
+    AuthService,
+    AlertService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ web/src/app/profile-logo/profile-logo.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../_services/auth.service';
+import {User} from '../_models/user';
+import {environment} from '../../environments/environment';
+
+@Component({
+  selector: 'app-profile-logo',
+  templateUrl: './profile-logo.component.html',
+  styleUrls: ['./profile-logo.component.scss']
+})
+export class ProfileLogoComponent implements OnInit {
+
+  user: User = {};
+  private username?: string;
+  shortName?: string;
+  loginUrl: string = environment.authUrl + '/login?returnUnion=unions';
+  registerUrl: string = environment.authUrl + '/register?returnUnion=unions';
+
+
+  constructor(
+    public authService: AuthService
+  ) {}
+
+  static generateShortName(user: User): string | undefined {
+    return user.username?.slice(0, 2).toUpperCase();
+  }
+
+  ngOnInit() {
+    if (this.authService.isLoggedIn()) {
+      this.authService.getUser().then(user => {
+        this.user = user;
+        this.username = user.username;
+        this.shortName = ProfileLogoComponent.generateShortName(user);
+      });
+    }
+  }
+
+}
